0a0034973c9cb1bf2b060e224e108e22943e03d4
SeyiAkinwale <72584469+SeyiAkinwale@users.noreply.github.com>
2023-07-18 01:37:06 +0000

    fixed parenthesis brace semi-colon with forEach

--- app.js
+++ app.js
@@ -1,72 +1,66 @@
 const todoInput = document.getElementById('todo-input')
 const addTaskButton = document.getElementById('add-task-btn')
 const todoList = document.getElementById('todo-list')
-console.log("What's going on");
-
+console.log("What's going on")
 //Add task
 const addTask = () => {
-   const taskText = todoInput.value.trim();
+  const taskText = todoInput.value.trim()
 
-    if (taskText !=='')
-    {
-      const taskItem = createTaskItem(taskText);
-      todoList.appendChild(taskItem);
-      todoInput.value = '';
-    }
+  if (taskText !== '') {
+    const taskItem = createTaskItem(taskText);
+    todoList.appendChild(taskItem);
+    todoInput.value = '';
   }
+}
 //Create new task items
 const createTaskItem = (taskText) => {
-    const taskItem = document.createElement('li');
-    taskItem.className = 'todo-item';
+  const taskItem = document.createElement('li');
+  taskItem.className = "todo-item"
 
-    const checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    checkbox.classList.add('checkbox');
+  const checkbox = document.createElement('input')
+  checkbox.type = 'checkbox'
+  checkbox.classList.add('checkbox');
 
-    const taskTextSpan = document.createElement('span');
-    taskTextSpan.textContent = taskText;
+  const taskTextSpan = document.createElement('span')
+  taskTextSpan.textContent = taskText;
 
-    const deleteBtn = document.createElement('button');
-    deleteBtn.textContent = 'Delete';
-    deleteBtn.classList.add('delete-btn');
-    deleteBtn.addEventListener('click', deleteTask);
+  const deleteBtn = document.createElement('button');
+  deleteBtn.textContent = 'Delete';
+  deleteBtn.classList.add('delete-btn');
+  deleteBtn.addEventListener('click', deleteTask);
 
-    taskItem.appendChild(checkbox);
-    taskItem.appendChild(taskTextSpan);
-    taskItem.appenChild(deleteBtn);
+  taskItem.appendChild(checkbox);
+  taskItem.appendChild(taskTextSpan);
+  taskItem.appendChild(deleteBtn);
 
-    return taskItem;
-  };
+  return taskItem;
+};
 
 //Delete tasks
-const deleteTask = (event) =>
-  {
-    const taskItem = event.target.parentNode;
-    todoList.removeChild(taskItem);
-  }
+const deleteTask = (event) => {
+  const taskItem = event.target.parentNode;
+  todoList.removeChild(taskItem);
+}
 
 //Cross out tasks
 const toggleTask = (event) => {
-    const taskItem = event.target.parentNode;
-    taskItem.classList.toggle('completed');
-  };
+  const taskItem = event.target.parentNode;
+  taskItem.classList.toggle('completed');
+};
 
 //Event listeners
 addTaskButton.addEventListener('click', addTask)
-todoInput.addEventListener('keydown', function (event))
-                           {
-  if (event.key === 'Enter')
-  {
+todoInput.addEventListener('keydown', function(event) {
+  if (event.key === 'Enter') {
     addTask();
   }
-}
+});
 
 todoList.addEventListener('change', toggleTask)
 //Examples of tasks
 const initialTasks = ['Buy Groceries', 'Pay bills', 'Cut Grass'];
 
-initialTasks.forEach((task)) =>
-{
+initialTasks.forEach((task) => {
   const taskItem = createTaskItem;
   todoList.appendChild(taskItem);
-}
+});
